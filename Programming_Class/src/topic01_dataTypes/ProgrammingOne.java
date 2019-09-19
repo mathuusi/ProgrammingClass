97f39d85604b289a5f8fd4493d21211a867064ec
Mathis Hermann <mathis.hermann@outlook.com>
2019-09-19 08:13:05 +0200

    new project

---
+++ Programming_Class/src/topic01_dataTypes/ProgrammingOne.java
@@ -0,0 +1,35 @@
+package topic01_dataTypes; // the programs location
+
+public class ProgrammingOne { // program itself
+	
+
+	public static void main(String[] args) {  // in this case the first (and only) method
+		// TODO Auto-generated method stub
+		
+		
+	// Fibonacci
+		
+		//try hard
+		
+		int a = 1;
+		int b = 1;
+		System.out.println(a);
+		System.out.println(b);
+
+		for (int i = 0; i < 5; i++) {
+			
+			a = a + b;
+			System.out.println(a);
+			b = a + b;
+			System.out.println(b);	
+			
+			
+			}
+		
+		float j = 13.5f;
+		
+		System.out.println(j);
+		
+		}
+	
+	}
